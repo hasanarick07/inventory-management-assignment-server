cf8bd93287f64fde8643343db3b79adcefc3b9b8
Arick Hasan <hasanarick000@gmail.com>
2022-05-08 18:12:44 +0600

    3rd

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  const hi = "hello Biker";
+  const hi = "hello Biker, What's up?";
   res.send(hi);
 });
 app.listen(port, () => {
