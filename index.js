af89706cccc58dde3b61025148d4bc906112f4b0
Arick Hasan <hasanarick000@gmail.com>
2022-05-08 12:56:42 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+app.use(cors());
+app.use(express.json());
+const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uqsst.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client
+      .db("biker-inventory")
+      .collection("product");
+    app.get("/product", async (req, res) => {
+      const products = await productCollection.find({}).toArray();
+      res.send(products);
+    });
+    app.get("/product/:id", async (req, res) => {
+      console.log(req.params.id);
+      const products = await productCollection.findOne({
+        _id: ObjectId(req.params.id),
+      });
+      res.send(products);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  const hi = "hello Biker";
+  res.send(hi);
+});
+app.listen(port, () => {
+  console.log("port running ", port);
+});
